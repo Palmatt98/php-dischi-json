70f7630b3b392b82e10c99930b1399bea44f78fb
Palmatt98 <palmierimatteo98@gmail.com>
2024-05-25 15:58:54 +0200

    bonus-1

--- script.js
+++ script.js
@@ -1,19 +1,59 @@
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
 	data() {
 		return {
 			dischi: [],
-		}
+			apiURL: "http://localhost/boolean/php-dischi-json/server.php",
+		};
 	},
 	methods: {
-
+		addLike(index) {
+			const data = {
+				discoIndex: index,
+				action: "like"
+			}
+			axios
+				.post(
+					this.apiURL, data,
+					{
+						headers: {
+							"Content-type": "multipart/form-data",
+						},
+					}
+				)
+				.then((response) => {
+					this.dischi = response.data.result;
+				})
+				.catch((error) => {
+					console.log("POTATO:", error);
+				});
+		},
+		removeLike(index) {
+			const data = {
+				discoIndex: index,
+				action: "dislike"
+			}
+			axios
+				.post(
+					this.apiURL, data,
+					{
+						headers: {
+							"Content-type": "multipart/form-data",
+						},
+					}
+				)
+				.then((response) => {
+					this.dischi = response.data.result;
+				})
+				.catch((error) => {
+					console.log("POTATO:", error);
+				});
+		},
 	},
-	created(){
-		axios.get("http://localhost/boolean/php-dischi-json/server.php")
-			.then(response =>{
-				this.dischi = response.data.result
-				console.log(this.dischi);
-			});
-	}
+	created() {
+		axios.get(this.apiURL).then((response) => {
+			this.dischi = response.data.result;
+		});
+	},
 }).mount("#app");
--- server.php
+++ server.php
@@ -5,6 +5,21 @@ header("Access-Control-Allow-Origin: *");
 $dischi_string = file_get_contents("dischi.json"); // string
 //trasforma la stringa in un array associativo
 $dischi = json_decode($dischi_string, true);
+
+if(isset($_POST["discoIndex"])){
+
+	$discoIndex = $_POST["discoIndex"];
+
+	if(isset($_POST["action"]) && $_POST["action"] == "like") {
+		$dischi[$discoIndex]["like"] = true;
+		file_put_contents("dischi.json", json_encode($dischi));
+	} else if(isset($_POST["action"]) && $_POST["action"] == "dislike") {
+		$dischi[$discoIndex]["like"] = false; 
+		file_put_contents("dischi.json", json_encode($dischi));
+	} 
+
+}
+
 //compongo i dati della risposta
 $response = [
 	"result" => $dischi,
